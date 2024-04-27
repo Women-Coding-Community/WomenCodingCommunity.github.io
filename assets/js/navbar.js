51a53cb2738f3b1ef74908a914f9008e7b6d5e9d
Airah Yusuff <airahyusuff@gmail.com>
2024-04-27 08:33:16 +0100

    update es6 syntax

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,19 +1,19 @@
-var controllerNavbar = (function (jQuery) {
+const controllerNavbar = (function (jQuery) {
   const NAVBAR_CLASS_ACTIVE = 'active-nav'
 
-  var selectActiveMenu = function () {
-    var pathSegments = jQuery(location).attr('pathname').split('/');
+  const selectActiveMenu = function () {
+    const pathSegments = jQuery(location).attr('pathname').split('/');
   
-    var firstPathSegment = pathSegments[pathSegments.length - 1];
-    var stringPathSegment = firstPathSegment.replaceAll('-', ' ');
+    const firstPathSegment = pathSegments[pathSegments.length - 1];
+    const stringPathSegment = firstPathSegment.replaceAll('-', ' ');
 
-    var activeLink;
-    var activeSubLink;
+    const activeLink;
+    const activeSubLink;
 
     if (!stringPathSegment) {
       activeLink = jQuery("a.nav-link:contains('Home')");
     } else {
-      var searchLinkText = stringPathSegment[0].toUpperCase() + stringPathSegment.slice(1, stringPathSegment.length);
+      const searchLinkText = stringPathSegment[0].toUpperCase() + stringPathSegment.slice(1, stringPathSegment.length);
       activeLink = jQuery("a.nav-link").filter(() => jQuery(this).text().trim() === searchLinkText)
       activeSubLink = jQuery("a.nav-link[href='" + firstPathSegment + "']")
     }
