ce49c73d7354f3f664deaf73ee00daf02e9dc849
Nevena Verbic <nevena.verbic@gmail.com>
2024-05-02 12:39:56 +0200

    Rename files used for test during the development

--- tools/run_mentors_automation.bat
+++ tools/run_mentors_automation.bat
@@ -2,7 +2,7 @@
 setlocal
 
 echo Enter arguments for Python script: FILE_PATH_MENTORS_XLSX FILE_PATH_MENTORS_YML MODE
-echo Example: ..\tools\mentors.xlsx ..\tools\mentors.yml a
+echo Example: mentors_test.xlsx mentors_test.yml a
 echo MODE: a - to append new mentors from the xlsx table to mentors.yml
 echo MODE: w - to create a new mentors.yml file with all mentors that are in the xlsx table
 call ..\.venv\Scripts\activate
