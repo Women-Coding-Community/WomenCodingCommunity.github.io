bc4fb1c8c6c94eeabdd23400f67bbbd95dea8508
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2024-05-05 18:41:45 +0200

    Fix test to use absolut path

--- tools/validate_yml_files_test.py
+++ tools/validate_yml_files_test.py
@@ -1,9 +1,11 @@
+import os
 import unittest
 
 import yml_utils as utils
 
 
 class ValidateYmlFilesTest(unittest.TestCase):
+
     def test_open_yml_success_sample(self):
         result = utils.read_yml_to_dict("samples/mentors.yml")
 
@@ -16,9 +18,11 @@ class ValidateYmlFilesTest(unittest.TestCase):
         self.assertEqual({}, result)
 
     def test_all_data_files(self):
+        data_folder = os.path.abspath(os.curdir).replace("tools", "_data/")
+
         file_names = ["mentors.yml", "announcement.yml", "books.yml", "collaborators.yml", "faq.yml", "footer.yml",
                       "index.yml", "navbar.yml", "programmes.yml", "resources.yml", "team.yml", "reviews.yml"]
 
         for file_name in file_names:
-            result = utils.read_yml_to_dict("../_data/" + file_name)
+            result = utils.read_yml_to_dict(data_folder + file_name)
             self.assertGreater(len(result), 0, f"_data/{file_name} is invalid.")
