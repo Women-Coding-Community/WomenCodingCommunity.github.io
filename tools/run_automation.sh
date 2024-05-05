c75ba31797924c39ce063a7ee869f501ec470a34
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2024-05-05 17:12:45 +0200

    Adjust comments in shell script

--- tools/run_automation.sh
+++ tools/run_automation.sh
@@ -5,12 +5,10 @@ python3 -m venv myenv
 source myenv/bin/activate
 
 # Install packages
-
 pip install -r requirements.txt
 
-#  - Enter the parameters: FILE_PATH_MENTORS_XLSX FILE_PATH_MENTORS_YML MODE
-#  - Example: samples/mentors.xlsx samples/mentors.yml a
-#  - mode "a" for APPEND new mentors from the xlsx table to the existing mentors.yml
-#  - mode "w" for WRITE all mentors from the xlsx table to mentors.yml
-
+# Enter the parameters: FILE_PATH_MENTORS_XLSX FILE_PATH_MENTORS_YML MODE
+# Example: samples/mentors.xlsx samples/mentors.yml a
+# mode "a" for APPEND new mentors from the xlsx table to the existing mentors.yml
+# mode "w" for WRITE all mentors from the xlsx table to mentors.yml
 python3 automation.py samples/mentors.xlsx samples/mentors.yml a
