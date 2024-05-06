e5164f42eff2d040f6d0b2af79393c1a7bd34937
Women Coding Community <167612780+WomenCodingCommunity@users.noreply.github.com>
2024-05-06 20:20:18 +0200

    Apply suggestions from code review

--- tools/run_automation.sh
+++ tools/run_automation.sh
@@ -7,7 +7,7 @@ source myenv/bin/activate
 # Install packages
 pip install -r requirements.txt
 
-# Enter the parameters: FILE_PATH_MENTORS_XLSX FILE_PATH_MENTORS_YML MODE
+# Enter the parameters: FILE_PATH_MENTORS_XLSX FILE_PATH_MENTORS_YML MODE SKIP_ROWS
 # Example: samples/mentors.xlsx samples/mentors.yml a
 # mode "a" for APPEND new mentors from the xlsx table to the existing mentors.yml
 # mode "w" for WRITE all mentors from the xlsx table to mentors.yml
