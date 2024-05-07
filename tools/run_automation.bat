74e3e386d9e455a5b4a0e9e8538f9623d8604cbc
Nevena Verbic <nevena.verbic@gmail.com>
2024-05-07 10:15:15 +0200

    Update batch script for windows and README

--- tools/run_automation.bat
+++ tools/run_automation.bat
@@ -4,28 +4,15 @@ echo Creating virtual environment...
 python -m venv ..\.venv
 
 echo Activating virtual environment...
-CALL ..\.venv\Scripts\activate
+call ..\.venv\Scripts\activate
 
 echo Installing dependencies...
 pip install -r requirements.txt
 
-echo Setup complete.
-
 setlocal
 echo Enter arguments for Python script: FILE_PATH_MENTORS_XLSX FILE_PATH_MENTORS_YML MODE SKIP_ROWS
-echo Example: mentors_test.xlsx mentors_test.yml a 0
+echo Example: mentors_test.xlsx mentors_test.yml a 1
 echo MODE: a - to append new mentors from the xlsx table to mentors.yml
 echo MODE: w - to create a new mentors.yml file with all mentors that are in the xlsx table
-call ..\.venv\Scripts\activate
-
-set /p args=Enter arguments for Python script:
-python automation.py %args%
-
-if %ERRORLEVEL% neq 0 (
-    echo Invalid arguments provided.
-    echo Please check the usage and try again.
-)
-call deactivate
-set args=
-endlocal
+python automation.py samples/mentors.xlsx samples/mentors.yml a 1
 @echo on
