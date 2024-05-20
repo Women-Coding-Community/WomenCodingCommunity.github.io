d87db4fcd40acbb1cb3e213c4de95e4e2dc37b7d
MitaliShah <mitalishaleenshah@gmail.com>
2024-05-20 18:25:27 -0500

    suggested changes

--- cypress/e2e/code-of-conduct.cy.js
+++ cypress/e2e/code-of-conduct.cy.js
@@ -1,17 +1,19 @@
 describe('Tests Mentorship Code of Conduct page', () => {
     const SLACK_LINK = "https://join.slack.com/t/womencodingcommunity/shared_invite/zt-2hpjwpx7l-rgceYBIWp6pCiwc0hVsX8A";
-
+    const BE_VISIBLE = 'be.visible';
     beforeEach(() => {
         cy.visit(`${Cypress.config().baseUrl}/mentorship-code-of-conduct`);
     });
 
     describe('Verify UI elements from Header and Main content', () => {
         it('should verify UI elements on the Code of Conduct page', () => {
-            cy.contains("Mentorship Code of Conduct", {matchCase: false}).should('be.visible');
-            cy.contains("Code Of Conduct for Mentees", {matchCase: false}).should('be.visible');
-            cy.contains("Code Of Conduct for Mentors", {matchCase: false}).should('be.visible');
-            cy.contains('Show More').should('be.visible');
-            cy.contains('Read').should('be.visible');
+            const IGNORE_CASE = {matchCase: false};
+            
+            cy.contains("Mentorship Code of Conduct", IGNORE_CASE).should(BE_VISIBLE);
+            cy.contains("Code Of Conduct for Mentees", IGNORE_CASE).should(BE_VISIBLE);
+            cy.contains("Code Of Conduct for Mentors", IGNORE_CASE).should(BE_VISIBLE);
+            cy.contains('Show More').should(BE_VISIBLE);
+            cy.contains('Read').should(BE_VISIBLE);
         });
 
         it('should find Slack link within #mentee-section and verify URL and text', () => {
@@ -25,13 +27,13 @@ describe('Tests Mentorship Code of Conduct page', () => {
     describe('Test "Show more," "Show less," for mentee and mentor', () => {
 
         it('should verify the existence of the anchor tag with mentee bottom learn more and Show More text', () => {
-            cy.get('a#btn-mentee-learn-more').should('be.visible').contains('Show More').should('be.visible');
+            cy.get('a#btn-mentee-learn-more').should(BE_VISIBLE).contains('Show More').should(BE_VISIBLE);
             cy.get('a#btn-mentee-learn-more').click();
-            cy.get('a#btn-mentee-show-less:contains("Show Less")', { defaultCommandTimeout: 1000 }).should('be.visible');
+            cy.get('a#btn-mentee-show-less:contains("Show Less")', { defaultCommandTimeout: 1000 }).should(BE_VISIBLE);
         });
 
         it('should verify the existence of the anchor tag with mentor bottom learn more and Show More text', () => {
-            cy.get('a#btn-mentor-learn-more').should('be.visible').contains('Show More').should('be.visible');
+            cy.get('a#btn-mentor-learn-more').should(BE_VISIBLE).contains('Show More').should(BE_VISIBLE);
             cy.get('a#btn-mentor-learn-more').click();
             cy.get('a#btn-mentee-show-less:contains("Show Less")', { timeout: 10000 }).should('exist');
         });
@@ -48,16 +50,16 @@ describe('Tests Mentorship Code of Conduct page', () => {
     describe('Verify all footer UI elements', () => {
 
         it('should verify the visibility of footer UI elements', () => {
-            cy.get('.brand').should('be.visible');
-            cy.contains("Women Coding Community is a not-for-profit organisation.").should('be.visible');
-            cy.contains('Follow Us').should('be.visible');
-            cy.contains('Join us on social media and stay tuned.').should('be.visible');
-            cy.contains('© 2024 Women Coding Community').should('be.visible');
+            cy.get('.brand').should(BE_VISIBLE);
+            cy.contains("Women Coding Community is a not-for-profit organisation.").should(BE_VISIBLE);
+            cy.contains('Follow Us').should(BE_VISIBLE);
+            cy.contains('Join us on social media and stay tuned.').should(BE_VISIBLE);
+            cy.contains('© 2024 Women Coding Community').should(BE_VISIBLE);
         });
 
         it('should verify LinkedIn link', () => {
             cy.get('.network a[href="https://www.linkedin.com/company/womencodingcommunity"]')
-                .should('be.visible')
+                .should(BE_VISIBLE)
                 .should('exist')
                 .should('have.attr', 'target', '_blank')
         });
@@ -71,28 +73,28 @@ describe('Tests Mentorship Code of Conduct page', () => {
 
         it('should verify GitHub link', () => {
             cy.get('.network a[href="https://github.com/WomenCodingCommunity"]')
-                .should('be.visible')
+                .should(BE_VISIBLE)
                 .should('exist')
                 .should('have.attr', 'target', '_blank')
         });
 
         it('should verify Instagram link', () => {
             cy.get('.network a[href="https://www.instagram.com/women_coding_community"]')
-                .should('be.visible')
+                .should(BE_VISIBLE)
                 .should('exist')
                 .should('have.attr', 'target', '_blank')
         });
 
         it('should verify Email link', () => {
             cy.get('.network a[href="mailto:london@womencodingcommunity.com"]')
-                .should('be.visible')
+                .should(BE_VISIBLE)
                 .should('exist')
                 .should('have.attr', 'target', '_blank')
         });
 
         it('should verify Twitter link', () => {
             cy.get('.network a[href="https://twitter.com/WCC_Community"]')
-                .should('be.visible')
+                .should(BE_VISIBLE)
                 .should('exist')
                 .should('have.attr', 'target', '_blank')
         });
