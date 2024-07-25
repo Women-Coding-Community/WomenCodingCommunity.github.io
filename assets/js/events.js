bb55c861ec302f76e56e93d983f811322add0cb2
Airah Yusuff <airahyusuff@gmail.com>
2024-07-25 18:38:52 +0100

    use js to handle show/hide of events based on expiration

--- assets/js/events.js
+++ assets/js/events.js
@@ -5,7 +5,40 @@ const eventsScript = (function(jQuery) {
     const CLASS_ACTIVE = 'active';
     const CLASS_HIDDEN = 'd-none';
 
+    const today = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+
+    const showFeaturedEvents = function() {
+        const events = jQuery('#featured-events .event-card');
+
+        const sortedEvents = events.sort(function(a, b) {
+            return jQuery(b).data('expiration') - jQuery(a).data('expiration');
+        });
+
+        let validEventCount = 0;
+        sortedEvents.each(function() {
+            const expiration = jQuery(this).data('expiration');
+            if (expiration >= today) {
+                if (validEventCount < 2) {
+                    jQuery(this).addClass(CLASS_ACTIVE);
+                    validEventCount++;
+                } else {
+                    jQuery(this).addClass(CLASS_HIDDEN);
+                }
+            } else {
+                jQuery(this).addClass(CLASS_HIDDEN);
+            }
+        });
+
+    }
+
     const showUpcomingEvents = function() {
+        jQuery('#upcoming-events .event-card').each(function() {
+            const expiration = jQuery(this).data('expiration');
+            if (expiration < today) {
+                jQuery(this).addClass(CLASS_HIDDEN);
+            }
+        });
+
         jQuery('#tab-past-events').removeClass(CLASS_ACTIVE);
         jQuery('#tab-upcoming-events').addClass(CLASS_ACTIVE);
 
@@ -14,6 +47,13 @@ const eventsScript = (function(jQuery) {
     }
 
     const showPastEvents = function() {
+        jQuery('#past-events .event-card').each(function() {
+            const expiration = jQuery(this).data('expiration');
+            if (expiration >= today) {
+                jQuery(this).addClass(CLASS_HIDDEN);
+            }
+        });
+
         jQuery('#tab-past-events').addClass(CLASS_ACTIVE);
         jQuery('#tab-upcoming-events').removeClass(CLASS_ACTIVE);
 
@@ -27,6 +67,8 @@ const eventsScript = (function(jQuery) {
     };
 
     const initEvents = function() {
+        showFeaturedEvents();
+
         pastEventsTab.click(function() {
             showPastEvents(jQuery(this));
         });
