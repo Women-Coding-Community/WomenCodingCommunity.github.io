9853394fd0cb7c04c3c9b1142f5dc09db651dd28
Airah Yusuff <airahyusuff@gmail.com>
2024-12-04 18:41:19 +0000

    add logic to handle months outside mentorship cycle

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,5 +1,7 @@
 const registrationController = (function(jQuery) {
     const CLASS_HIDDEN = 'd-none';
+    const MENTORSHIP_OPEN_MONTH = 3; //March
+    const MENTORSHIP_CLOSE_MONTH = 11; //November
 
     const checkAdHocSessions = function() {
         jQuery('.card-mentor').each(function() {
@@ -14,8 +16,13 @@ const registrationController = (function(jQuery) {
                 const currentDate = new Date();
                 const currentDay = currentDate.getDate();
                 const currentMonth = currentDate.getMonth() + 1;
+
+                const isMentorshipCycle = (
+                    currentMonth >= MENTORSHIP_OPEN_MONTH && 
+                    currentMonth <= MENTORSHIP_CLOSE_MONTH
+                );
                 
-                if (currentDay <= daysOpen) {
+                if (currentDay <= daysOpen && isMentorshipCycle) {
                     if (mentorType !== 'long-term') {
                         if (mentorAvailability.includes(currentMonth.toString())) {
                             jQuery(`#registration-link-${mentorIndex}`).removeClass(CLASS_HIDDEN);
