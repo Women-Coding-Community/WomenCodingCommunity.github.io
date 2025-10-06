e5003c8c1c0f80f874cd8d4186db24d15e72c509
Airah Yusuff <airahyusuff@gmail.com>
2025-10-06 19:49:30 +0100

    update test to reflect additional sample mentor added to sheet

--- tools/tests/automation_functional_test.py
+++ tools/tests/automation_functional_test.py
@@ -7,6 +7,7 @@ from automation_mentors import run_automation, read_yml_file, WriteMode
 
 MENTOR_2 = "Mentor2 Name"
 MENTOR_3 = "Mentor3 Name"
+MENTOR_4 = "Mentor4 Name"
 
 def test_write_mentors_skip_zero_rows(monkeypatch):
 
@@ -19,9 +20,10 @@ def test_write_mentors_skip_zero_rows(monkeypatch):
     run_automation()
 
     result = read_yml_file(tmp_filename)
-    assert len(result) == 2, f"Expected to write 2 mentors but added {len(result)}"
+    assert len(result) == 3, f"Expected to write 3 mentors but added {len(result)}"
     assert MENTOR_2 == result[0]['name'], f"Expected content to be {MENTOR_2} but got '{result[0]['name']}'"
-    assert MENTOR_3 == result[1]['name'], f"Expected content to be {MENTOR_3} but got '{result[0]['name']}'"
+    assert MENTOR_3 == result[1]['name'], f"Expected content to be {MENTOR_3} but got '{result[1]['name']}'"
+    assert MENTOR_4 == result[2]['name'], f"Expected content to be {MENTOR_4} but got '{result[2]['name']}'"
 
     # Clean up the temporary file
     os.remove(tmp_filename)
